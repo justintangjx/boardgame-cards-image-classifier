b341581d9902b24a421952e704dad3ef081f1a5a
justin tang <justintang@justins-MacBook-Pro.local>
2020-01-21 14:32:52 +0800

    setup list of cards to classify

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  const [originalText, nextStateChange] = useState('hehe FIRST-IN')
-  return (
-    <View style={styles.container}>
-      <Text>{originalText}</Text>
-      <Button title="change text" onPress={ () => nextStateChange('changed to LALALAND') }/>
+  const [newCardInput, toAdd] = useState('');
+  const cardInputHandler = enteredText => {
+    toAdd(enteredText);
+  };
 
+  const [cards, addCardToList] = useState([]);
+  const addCardHandler = () => {
+    addCardToList(cards => [...cards, newCardInput])
+  };
+  return (
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <TextInput
+          placeholder="cards to classify"
+          style={styles.textInput}
+          onChangeText={cardInputHandler}
+          value={newCardInput}
+        />
+        <Button title="ADD" onPress={addCardHandler} />
+      </View>
+      <View>
+      {cards.map((card) => <View key={card} style={styles.listItem}>  <Text> {card} </Text> </View>)}
+      </View>
     </View>
   );
 }
@@ -15,8 +38,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  textInput: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 2,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
